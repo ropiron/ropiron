9c4cebd02691697ef5be54f4bb1dfab061dc2a5a
ropiron <hirothedon@tutanota.com>
2023-01-05 06:36:30 +0900

    changed init.el and installed elisp files

---
+++ elpa/mozc-20221214.1435/mozc-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from mozc.el  -*- no-byte-compile: t -*-
+(define-package "mozc" "20221214.1435" "minor mode to input Japanese with Mozc" 'nil :commit "cebb883f5d7d8d6dec05c6e2ed4096d0e8ff8c9f" :keywords '("mule" "multilingual" "input method"))
---
+++ elpa/mozc-popup-20150224.34/mozc-popup-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from mozc-popup.el  -*- no-byte-compile: t -*-
+(define-package "mozc-popup" "20150224.34" "Mozc with popup" '((popup "0.5.2") (mozc "0")) :commit "f0684b875a7427ec08f8df13939a486e5d5cf420" :authors '(("Daisuke Kobayashi" . "d5884jp@gmail.com")) :maintainer '("Daisuke Kobayashi" . "d5884jp@gmail.com") :keywords '("i18n" "extentions"))
